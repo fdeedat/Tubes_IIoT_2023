935181b64916dae5f57409091d04ebcdcad32a49
rega1001 <90404466+rega1001@users.noreply.github.com>
2023-05-28 17:13:07 +0700

    Frontend login dan register beres

--- Web/public/scriptPrak.js
+++ Web/public/scriptPrak.js
@@ -5,7 +5,11 @@ socket.connect(`http://${host}:8000`);
 // DOM things
 const btn = document.getElementById('button');
 const btnTitle = document.getElementById("buttonTitle");
+<<<<<<< Updated upstream
 btnTitle.innerHTML = "OFF";
+=======
+btnTitle.innerHTML = "DISCONNECTED";
+>>>>>>> Stashed changes
 
 // Callback function for click event
 btn.addEventListener('click', sendOverSocket);
@@ -20,12 +24,20 @@ async function sendOverSocket() {
             state: 1,
         });
         currentState = 1;
+<<<<<<< Updated upstream
         btnTitle.innerHTML = "ON";
+=======
+        btnTitle.innerHTML = "CONNECTED";
+>>>>>>> Stashed changes
     }else{
         await socket.emit('buttonState', {
             state: 0
         });
         currentState=0;
+<<<<<<< Updated upstream
         btnTitle.innerHTML = "OFF";
+=======
+        btnTitle.innerHTML = "DISCONNECTED";
+>>>>>>> Stashed changes
     }
 };
